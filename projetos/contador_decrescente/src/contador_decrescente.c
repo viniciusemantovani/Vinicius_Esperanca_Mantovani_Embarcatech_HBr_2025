900ac26476b962da313bad2f05378589c1ddfdfe
Vinicius <vinemprego@gmail.com>
2025-04-13 14:30:11 -0300

    Primeira versão do contador decrescente, funcional (problema de rebounce de botões)

--- projetos/contador_decrescente/src/contador_decrescente.c
+++ projetos/contador_decrescente/src/contador_decrescente.c
@@ -10,12 +10,23 @@
 const uint8_t BUTTON_A = 5;
 const uint8_t BUTTON_B = 6;
 
+absolute_time_t actual_time = 0;
+uint8_t count_secs = 9;
 uint8_t countB = 0;
+uint8_t counting = 0; // 0 - não conta; 1 - contando.
 //----------------------------------------------------------------------------------
 //DISPLAY:
 
 const uint8_t I2C_SDA = 14;
 const uint8_t I2C_SCL = 15;
+
+uint8_t ssd[ssd1306_buffer_length];
+struct render_area frame_area = {
+  start_column : 0,
+  end_column : ssd1306_width - 1,
+  start_page : 0,
+  end_page : ssd1306_n_pages - 1
+};
 
 /**
  * @brief Escreve um conjunto de strings no display dada uma mensagem.
@@ -48,18 +59,39 @@ void organizeStrings(char *str1, char *str2, char *str3, uint8_t *ssd, struct re
 
 void dispCountNum(int num, uint8_t *ssd, struct render_area frame_area){
   char str_resultado[17];
-  sprintf(str_resultado, "       %d       ", num);
-  organizeStrings("     COUNT    ", str_resultado, "                ", ssd, frame_area);
+  char str_B_clicks[17];
+  sprintf(str_resultado, " Count:  %d      ", num);
+  sprintf(str_B_clicks,  " Clicks: %d      ", countB);
+  organizeStrings("    Counter     ", str_resultado, str_B_clicks, ssd, frame_area);
 }
 
 //----------------------------------------------------------------------------------
 
-void ButtonBHandler(){
+void ButtonHandler(){
   static absolute_time_t deb_time_B = 0;
-  if(absolute_time_diff_us(deb_time_B, get_absolute_time()) > 300){
-    countB++;
-    deb_time_B = get_absolute_time();
+  static absolute_time_t deb_time_A = 0;
+
+  if(!gpio_get(BUTTON_B)){
+    if(absolute_time_diff_us(deb_time_B, get_absolute_time()) > 2000){
+      countB++;
+      deb_time_B = get_absolute_time();
+      if(counting == 1){
+        dispCountNum(count_secs, ssd, frame_area);
+      }
+    }
+
+  } else if (!gpio_get(BUTTON_A)){
+    if(absolute_time_diff_us(deb_time_A, get_absolute_time()) > 9000){
+      count_secs = 9;
+      countB = 0;
+      counting = 1;
+      deb_time_B = get_absolute_time();
+      actual_time = get_absolute_time();
+      dispCountNum(count_secs, ssd, frame_area);
+
+    }
   }
+
 }
 
 int main()
@@ -76,30 +108,29 @@ int main()
     gpio_set_dir(BUTTON_B, GPIO_IN);
     gpio_pull_up(BUTTON_B);
     
-    gpio_set_irq_enabled_with_callback(BUTTON_B, 0x04, 1, &ButtonBHandler);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, 0x04, 1, &ButtonHandler);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, 0x04, 1, &ButtonHandler);
 
     i2cInitDisplay(I2C_SDA, I2C_SCL); // Inicialização do i2c e do display OLED
     // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
-    struct render_area frame_area = {
-      start_column : 0,
-      end_column : ssd1306_width - 1,
-      start_page : 0,
-      end_page : ssd1306_n_pages - 1
-    };
+
     calculate_render_area_buffer_length(&frame_area);
     // Limpa o display
-    uint8_t ssd[ssd1306_buffer_length];
+    
     clearDisplay(ssd, frame_area);
 
-    absolute_time_t actual_time = 0;
-    uint8_t count_secs = 9;
+    dispCountNum(count_secs, ssd, frame_area);
 
     while (true) {
-      printf("%d",countB);
-      dispCountNum(count_secs, ssd, frame_area);
-      while(absolute_time_diff_us(actual_time, get_absolute_time()) < 1000000);
-      actual_time = get_absolute_time();
-      count_secs--;
-      if(count_secs == 255) count_secs = 9;
+      if(counting){
+        dispCountNum(count_secs, ssd, frame_area);
+        while(absolute_time_diff_us(actual_time, get_absolute_time()) < 1000000);
+        actual_time = get_absolute_time();
+        count_secs--;
+        if(count_secs == 0){
+          dispCountNum(count_secs, ssd, frame_area);
+          counting = 0;
+        }
+      }
     }
 }
