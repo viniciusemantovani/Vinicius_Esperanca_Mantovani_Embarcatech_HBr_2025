4b0a80c31235a254d6170719cef75df0323dee23
Vinicius <vinemprego@gmail.com>
2025-05-01 15:24:28 -0300

    Galton BOard funcional, histograma com problema!

--- projetos/galton_board/src/galton_board.c
+++ projetos/galton_board/src/galton_board.c
@@ -10,6 +10,8 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 
+#define PIN_LINES 10
+
 ssd1306_t disp; // Instância do display.
 
 // Estrutura da bola e dos pinos:
@@ -20,8 +22,8 @@ typedef struct ball{
 
 //  Estrutura das barras do histograma:
 typedef struct bar{
-    uint8_t xmin;
-    uint8_t xmax;
+    uint8_t ymin;
+    uint8_t ymax;
     uint8_t num_bolas;
 } bar;
 
@@ -33,10 +35,18 @@ typedef struct bar{
 void gen_pins_position(ball *pins, bar *bars){
     ball init_pos = {8,30}; // Centro em (9,31)
     int count = 0;
-    for(int i = 0; i < 16; i++){
+    for(int i = 0; i < PIN_LINES; i++){
         ball aux = init_pos;
         for(int j = 0; j < i + 1; j++){
             pins[count] = aux;
+            if(i == PIN_LINES-1){ // Última linha. ADD bars.
+                bars[j].ymax = aux.y + 4;
+                bars[j].ymin = aux.y + 1;
+                if(j == PIN_LINES-1){
+                    bars[j+1].ymax = aux.y;
+                    bars[j+1].ymin = aux.y - 3;
+                }                
+            }
             ssd1306_draw_square(&disp, pins[count].x, pins[count].y, 2, 2);
             aux.y -= 4; // Começa a 2 de distância do centro do esquerdo.
             count++;
@@ -45,6 +55,9 @@ void gen_pins_position(ball *pins, bar *bars){
         init_pos.y = init_pos.y + 2; // Começa a 2 de distância do centro do superior à esquerda dele.
     }
 
+    for(int i= 0; i < PIN_LINES + 1; i++ ){
+        printf("bars %d = (%d <-> %d)", i, bars[i].ymin, bars[i].ymax);
+    }
     ssd1306_show(&disp);
 }
 
@@ -74,8 +87,8 @@ void start_ball(ball *ballx){
  * @brief Verifica se a bola colidiu com um pino e move horizontalmente caso sim.
  * @param ballx ponteiro para bola a ser verificada e modificada.
  */
-void handle_colision(ball *ballx, ball *pins){
-    for(int i = 0; i < 136; i++){
+void handle_colision(ball *ballx, ball *pins, int pins_total){
+    for(int i = 0; i < pins_total; i++){
         if(ballx->y <= pins[i].y+1 && ballx->y+1 >= pins[i].y && ballx->x+1 == pins[i].x){
             bool direction = gen_rand_direction();
             ballx->y = direction ? ballx->y - 2 : ballx->y + 2;
@@ -83,58 +96,47 @@ void handle_colision(ball *ballx, ball *pins){
     }
 }
 
-/**TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- * TESTAR
- */
-
 /**
  * @brief Atualiza posição da bola.
  * @param ballx ponteiro para a bola a ser modificada.
  */
-void update_ball(ball *ballx, ball *pins, bar *bars){
-    // if(ballx->x >= 63){
-    //     for(int i = 0; i <= 8; i++){
-    //         if(ballx->x > bars[i].xmin && ballx->x < bars[i].xmax){
-    //             bars[i].num_bolas++;
-    //             printf("caiu no %u, número bolas: %u  ", bars[i].xmin, bars[i].num_bolas);
-    //         }
-    //     }
-    //     ballx->x = 0;
-    //     ballx->y = 0;
-    //     return;
-    // }
+void update_ball(ball *ballx, ball *pins, bar *bars, int pins_total){
+
+    // Histograma:
+    if(ballx->x >= 63){
+        for(int i = 0; i <= PIN_LINES+1; i++){
+            if(ballx->y > bars[i].ymin && ballx->y < bars[i].ymax){
+                bars[i].num_bolas++;
+                ssd1306_draw_square(&disp, 127 - bars[i].num_bolas, bars[i].ymin+1, 1, 2);
+            }
+        }
+
+        ssd1306_clear_square(&disp, ballx->x, ballx->y, 2, 2); 
+        ssd1306_show(&disp);
+        ballx->x = 0;
+        ballx->y = 30;
+        return;
+    }
 
     ssd1306_clear_square(&disp, ballx->x, ballx->y, 2, 2);
 
     ballx->x++;
-    handle_colision(ballx, pins);
+    handle_colision(ballx, pins, pins_total);
     ssd1306_draw_square(&disp, ballx->x, ballx->y, 2, 2);
     ssd1306_show(&disp);
 }
 
 int main()
 {
+    int pins_total = 0;
+    for(int i = 0; i < PIN_LINES; i++){
+        pins_total += i + 1;
+    }
+
     //  Pinos:
-    ball pins_position[136]; // A última linha tem no máximo 16 pinos
+    ball pins_position[pins_total];
     // Barras do histograma:
-    bar bars[17]; // No máximo 17 barras.
+    bar bars[PIN_LINES+1];
 
     stdio_init_all();
 
@@ -161,14 +163,14 @@ int main()
     bool direction;
     ball bola;
     start_ball(&bola);
-    // sleep_ms(5000);
+    sleep_ms(5000);
     gen_pins_position(pins_position, bars);
     // for(int i = 0; i < 136; i++){
     //     printf("pos pin %d = (%d, %d)\n\n", i, pins_position[i].x, pins_position[i].y);
 
     // }
     while (true) {
-        update_ball(&bola, pins_position, bars);
+        update_ball(&bola, pins_position, bars, pins_total);
         // printf("bola x: %u  y: %u\n", bola.x, bola.y);
     }
 }
