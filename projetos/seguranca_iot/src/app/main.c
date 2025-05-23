7a82047d25161737770f70be71fbf5a15bee3f54
Vinicius <vinemprego@gmail.com>
2025-05-22 21:09:35 -0300

    update programa Mqtt

--- projetos/seguranca_iot/src/app/main.c
+++ projetos/seguranca_iot/src/app/main.c
@@ -5,16 +5,20 @@
 #include "mqtt_comm.h"    // Header file com as declarações locais
 #include "lwip/apps/mqtt.h"       // Biblioteca MQTT do lwIP
 
+#define WIFI_SSID "HSC"
+#define WIFI_PASS "humbertosc744"
+#define PC_IP "192.168.31.169"
+
 int main()
 {
     stdio_init_all();
 
     sleep_ms(5000);
 
-    connect_to_wifi("HBR Guest", "Visit@8523");
-    
+    connect_to_wifi(WIFI_SSID, WIFI_PASS);
+    mqtt_setup("RP_PI", PC_IP , NULL, NULL);
+    mqtt_comm_publish("test", "mensagem\0", 9);
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
